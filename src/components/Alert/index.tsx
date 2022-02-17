6521bba48055787a7bd5be115247f3885ccf94d1
CCastro01 <alvescastro00cca13@gmail.com>
2022-02-17 01:38:28 -0300

    [IOB-001] Creating login page

---
+++ src/components/Alert/index.tsx
@@ -0,0 +1,9 @@
+import './style.scss'
+
+export default function AlertComponent(){
+    return(
+        <div className="alert_balloon">
+            <p>Email e/ou senha incorretos.</p>
+        </div>
+    )
+}
